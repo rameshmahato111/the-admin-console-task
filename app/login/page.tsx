a47224db3908a4e5a5dfd22eb5efc5e084139fde
Ramesh <ramesh@Rameshs-MacBook-m3.local>
2025-11-16 21:41:08 +0545

    login page is added

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import { LoginForm } from '@/components/story2/login'
+import React from 'react'
+
+const page = () => {
+  return (
+   
+     <LoginForm/>
+
+
+    
+  )
+}
+
+export default page
---
+++ components/story2/login.tsx
@@ -0,0 +1,73 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+
+export function LoginForm({
+  className,
+
+}: React.ComponentProps<"div">) {
+  return (
+    <section className="max-w-lg mx-auto py-20">
+    <div className={cn("flex flex-col gap-6", className)}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4">
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center justify-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field className="flex flex-col gap-2">
+                <Button type="submit" className="w-full">Login</Button>
+                <Button variant="outline" type="button" className="w-full">
+                  Login with Google
+                </Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+    </section>
+  )
+}
